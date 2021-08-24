79b95ea98e74826dc58a9434ec65480361937b56
DESKTOP-KJQ65RF\Sachin <sachinsenanayake1996@gmail.com>
2021-08-24 16:49:58 +0530

    2 commit

---
+++ pages/RegisterPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RegisterPage extends React.Component{
+    render(){
+      return(
+        <View>
+          <Text> Industrial </Text>
+        </View>
+      )
+    }
+  }
+  
+  export default RegisterPage;
